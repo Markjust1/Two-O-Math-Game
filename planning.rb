6fd6fe96cef1dde43636f43aa65f19ea175dd9e8
Markjust1 <novikov711@gmail.com>
2022-06-27 12:31:21 -0600

    initial commit

---
+++ planning.rb
@@ -0,0 +1,18 @@
+class Question
+  #Takes in game logic, what all classes have in common
+
+  #carries the game loop - defines who currnt player is
+
+  #takes in arithmetic calculation
+
+end
+
+class Player1 include Player
+  #carries name and lives
+  #can answer prompts when its turn
+end
+
+class Player2 include Player
+  #carries name and lives
+  #can answer prompts when its turn
+end
